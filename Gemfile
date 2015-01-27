bc1b08182571ccab1c4d8be22a719d5eccc3857f
bartek86 <bartek.lesniak86@gmail.com>
2015-01-27 02:16:54 +0000

    validations

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails', '4.0.0.beta2'
 gem 'turbolinks',   '2.3.0'
 gem 'jbuilder',     '2.2.3'
 gem 'sdoc',         '0.4.0', group: :doc
+gem 'validates_timeliness', '~> 3.0.2'
 
 group :development, :test do
   gem 'sqlite3',     '1.3.9'
--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,3 +1,19 @@
 class Rent < ActiveRecord::Base
   belongs_to :car
+  validates :from_date, :to_date, :presence => true
+  before_validation :available, :on => :create
+  
+  validates :client, length: {minimum: 3}, :presence => true
+  validates_datetime :from_date, :on => :create, :on_or_after => :now
+  validates_datetime :to_date, :on => :create, :after => :from_date
+
+
+  def available
+    @car = Car.find(self.car_id)
+    rented = @car.rents.where("(from_date <= :from_date and to_date >= :from_date) or (from_date <= :to_date and to_date >= :to_date)",
+                                      {:from_date => self.from_date, :to_date => self.to_date}).count
+    
+    return(rented == 0)
+  end
+  
 end
